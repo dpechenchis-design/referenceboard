02b7ed314ec6b4b9f3abb164c399ae1c8614b1f8
dpechenchis-design <dpechenchis@gmail.com>
2026-07-22 18:22:01 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,1 @@
-import { send, readBody } from './_store.js';
 
-export default async function handler(req, res){
-  // Захист прибрано: вхід дозволено завжди.
-  return send(res, 200, { ok: true });
-}
